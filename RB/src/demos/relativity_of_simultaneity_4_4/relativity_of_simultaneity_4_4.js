2a38f2caaa6ac6eb474b5dfcf82105a64805de92
Robert Bennett <rltbennett@icloud.com>
2020-05-13 16:21:13 -0400

    I watch the data flow; I feel d3 seep through me. But each time I feel at one with d3, I soon realize just how far from perfection I really am. Made everything in 4.4 fully d3-ish Learned the difference between regular functions and arrow functions w.r.t. `this`

--- RB/src/demos/relativity_of_simultaneity_4_4/relativity_of_simultaneity_4_4.js
+++ RB/src/demos/relativity_of_simultaneity_4_4/relativity_of_simultaneity_4_4.js
@@ -58,8 +58,6 @@ const subcanvases = canvas
 	.attr("transform", d => `translate(0, ${d.originY})`)
 	.classed("subcanvas", true);
 
-console.log(subcanvases);
-
 // Photon goes from center of train to end of train over the course of the animation
 function axDistTraveled({ fracOfC }) {
 	return (fracOfC * AX_TRAIN_WIDTH) / 2;
@@ -87,23 +85,7 @@ function transAxisToCanvas(canvasInfo, { x, y, dx, dy }) {
 	};
 }
 
-function x1(d) {
-	return d.p1.x;
-}
-
-function y1(d) {
-	return d.p1.y;
-}
-
-function x2(d) {
-	return d.p2.x;
-}
-
-function y2(d) {
-	return d.p2.y;
-}
-
-function addTrainAndLightSources(canvasInfo) {
+function _getTrainAndLightSourcesData(canvasInfo) {
 	const trainXMin = AX_MID_X - AX_TRAIN_WIDTH / 2;
 	const trainYMin = AX_MID_Y - AX_TRAIN_HEIGHT / 2;
 
@@ -141,120 +123,75 @@ function addTrainAndLightSources(canvasInfo) {
 
 	const lsRadius = Math.min(lsCanvasRadiusX, lsCanvasRadiusY);
 
-	// const trainCar = d3
-	// 	.select(this)
-	// 	.selectAll()
-	// 	.data([{ x0: trainMinCanvasCoords.x }])
-	// 	.enter()
-	// 	.append("rect")
-	// 	.attr("x", trainMinCanvasCoords.x)
-	// 	.attr("y", trainMinCanvasCoords.y)
-	// 	.attr("width", trainCanvasWidth)
-	// 	.attr("height", trainCanvasHeight)
-	// 	.attr("fill", "#4ff8")
-	// 	.classed("train-train-car", true);
-
-	// // console.log(lsCanvasCenterX, lsCanvasCenterY);
-	// const lightSource = d3
-	// 	.select(this)
-	// 	.selectAll()
-	// 	.data([{ x0: lsCanvasCenterX }])
-	// 	.enter()
-	// 	.append("circle")
-	// 	.attr("cx", d => d.x0)
-	// 	.attr("cy", lsCanvasCenterY)
-	// 	.attr("r", lsRadius)
-	// 	.attr("fill", "#fb3f")
-	// 	.attr("stroke", "#ffff")
-	// 	.classed("train-light-source", true);
-
 	const photonRadius = lsRadius / 2;
-
-	// const photons = d3
-	// 	.select(this)
-	// 	.selectAll()
-	// 	.data([
-	// 		{ x0: lsCanvasCenterX, x1: trainMinCanvasCoords.x },
-	// 		{ x0: lsCanvasCenterX, x1: trainMaxCanvasCoords.x },
-	// 	])
-	// 	.enter()
-	// 	.append("circle")
-	// 	.attr("cx", d => d.x0)
-	// 	.attr("cy", lsCanvasCenterY)
-	// 	.attr("r", photonRadius)
-	// 	.attr("fill", "#fd0f")
-	// 	.attr("stroke", "#ffff")
-	// 	.classed("train-photon", true);
 
 	const circleTopLevelAttrs = {
 		shape: "circle",
 		x0: lsCanvasCenterX,
 	};
 
-	const circleAttrAttrs = {
+	const _circleAttrAttrs = {
 		cx: lsCanvasCenterX,
 		cy: lsCanvasCenterY,
+	};
+
+	const lightSourceAttrs = {
+		..._circleAttrAttrs,
 		r: lsRadius,
+		fill: "#fb3f",
+		stroke: "#ffff",
 	};
 
 	const photonAttrAttrs = {
-		...circleAttrAttrs,
+		..._circleAttrAttrs,
 		r: photonRadius,
 		fill: "#fd0f",
 		stroke: "#ffff",
 	};
 
-	return d3
-		.select(this)
-		.selectAll()
-		.data([
-			{
-				type: "train",
-				class: "train-car",
-				shape: "rect",
-				x0: trainMinCanvasCoords.x,
-				attrs: {
-					x: trainMinCanvasCoords.x,
-					y: trainMinCanvasCoords.y,
-					width: trainCanvasWidth,
-					height: trainCanvasHeight,
-					fill: "#4ff8",
-				},
+	return [
+		{
+			type: "train",
+			class: "train-car",
+			canvas: canvasInfo,
+			shape: "rect",
+			x0: trainMinCanvasCoords.x,
+			attrs: {
+				x: trainMinCanvasCoords.x,
+				y: trainMinCanvasCoords.y,
+				width: trainCanvasWidth,
+				height: trainCanvasHeight,
+				fill: "#4ff8",
 			},
-			{
-				type: "lightSource",
-				class: "train-light-source",
-				...circleTopLevelAttrs,
-				attrs: {
-					...circleAttrAttrs,
-					fill: "#fb3f",
-					stroke: "#ffff",
-				},
-			},
-			{
-				type: "leftPhoton",
-				class: "train-photon",
-				...circleTopLevelAttrs,
-				attrs: photonAttrAttrs,
-			},
-			{
-				type: "rightPhoton",
-				class: "train-photon",
-				...circleTopLevelAttrs,
-				attrs: photonAttrAttrs,
-			},
-		])
-		.enter()
-		.each(d => {
-			var d3Obj = d3.select(this).append(d.shape).classed(d.class, true);
-
-			Object.keys(d.attrs).forEach(key => {
-				d3Obj = d3Obj.attr(key, d.attrs[key]);
-			});
-		});
+		},
+		{
+			type: "lightSource",
+			class: "train-light-source",
+			canvas: canvasInfo,
+			...circleTopLevelAttrs,
+			attrs: lightSourceAttrs,
+		},
+		{
+			type: "photon",
+			class: "train-photon",
+			canvas: canvasInfo,
+			...circleTopLevelAttrs,
+			attrs: photonAttrAttrs,
+			direction: "left",
+		},
+		{
+			type: "photon",
+			class: "train-photon",
+			canvas: canvasInfo,
+			...circleTopLevelAttrs,
+			attrs: photonAttrAttrs,
+			direction: "right",
+		},
+	];
 }
 
-function addTracks(canvasInfo) {
+function _getTracksData(canvasInfo) {
+	console.log(canvasInfo);
 	const nTiesVisible = 9;
 	const lineExtendPastFrac = 0.05;
 
@@ -271,7 +208,7 @@ function addTracks(canvasInfo) {
 	);
 
 	// make ties
-	const tiesCanvasCoords = [];
+	const data = [];
 	for (var i = 0; i < nTiesTotal; i++) {
 		const tieAxX = trackInteriorAxMinX + i * axDistBtwnTies;
 
@@ -279,57 +216,80 @@ function addTracks(canvasInfo) {
 			{ x: tieAxX, y: AX_MIN_Y },
 			{ x: tieAxX, y: AX_MAX_Y },
 		].map(p => transAxisToCanvas(canvasInfo, p));
-		tiesCanvasCoords.push({ p1, p2, baseX: p1.x });
+
+		data.push({
+			type: "railroadTie",
+			class: "railroad-tie",
+			canvas: canvasInfo,
+			x0: p1.x,
+			shape: "line",
+			attrs: {
+				x1: p1.x,
+				y1: p1.y,
+				x2: p2.x,
+				y2: p2.y,
+				stroke: "#997f39",
+				"stroke-width": 20,
+			},
+		});
 	}
 
-	const ties = d3
-		.select(this)
-		.selectAll()
-		.data(tiesCanvasCoords)
-		.enter()
-		.append("line")
-		.attr("x1", x1)
-		.attr("y1", y1)
-		.attr("x2", x2)
-		.attr("y2", y2)
-		.style("stroke", "#997f39")
-		.style("stroke-width", 20)
-		.classed("railroad-tie", true);
-
 	// make rails
-	const railsCanvasCoords = [];
 	[trackInteriorAxMinY, trackInteriorAxMaxY].forEach(railY => {
 		const [p1, p2] = [
 			{ x: AX_MIN_X, y: railY },
 			{ x: AX_MAX_X, y: railY },
 		].map(p => transAxisToCanvas(canvasInfo, p));
-		railsCanvasCoords.push({ p1, p2 });
+
+		data.push({
+			type: "railroadRail",
+			class: "railroad-rail",
+			canvas: canvasInfo,
+			shape: "line",
+			attrs: {
+				x1: p1.x,
+				y1: p1.y,
+				x2: p2.x,
+				y2: p2.y,
+				stroke: "white",
+				"stroke-width": 5,
+			},
+		});
 	});
 
-	const rails = d3
-		.select(this)
-		.selectAll()
-		.data(railsCanvasCoords)
-		.enter()
-		.append("line")
-		.attr("x1", x1)
-		.attr("y1", y1)
-		.attr("x2", x2)
-		.attr("y2", y2)
-		.attr("stroke", "white")
-		.attr("stroke-width", 5)
-		.classed("railroad-rail", true);
-
-	return { ties, rails };
+	return data;
 }
 
-// subcanvas.each((d, i) => addTracks());
+function _addGraphicalObjs(subcanvases, dataFunc) {
+	return subcanvases
+		.selectAll()
+		.data(dataFunc)
+		.enter()
+		.append(function (d) {
+			return d3.create(`svg:${d.shape}`).node();
+		})
+		.each(function (d) {
+			var d3Obj = d3.select(this).classed(d.class, true);
 
-// const trackLines = addTracks(canvas);
-// console.log(trackLines);
-const trackLines = subcanvases.each(addTracks);
-const xxx = subcanvases.each(addTrainAndLightSources);
-console.log(subcanvases);
+			Object.keys(d.attrs).forEach(key => {
+				d3Obj = d3Obj.attr(key, d.attrs[key]);
+			});
+
+			return d3Obj;
+		});
+}
+
+function addTracks(subcanvases) {
+	return _addGraphicalObjs(subcanvases, _getTracksData);
+}
+
+function addTrainAndLightSources(subcanvases) {
+	return _addGraphicalObjs(subcanvases, _getTrainAndLightSourcesData);
+}
+
+const tracks = addTracks(subcanvases);
+console.log(tracks);
+const trainObjs = addTrainAndLightSources(subcanvases);
 
 // eslint-disable-next-line no-unused-vars
 function updateTrainSpeed(speed) {
@@ -348,39 +308,68 @@ function updateTrainSpeed(speed) {
 }
 
 // eslint-disable-next-line no-unused-vars
-function beginAnimationOnCanvas(canvasInfo) {
+function beginAnimation() {
 	document.getElementById("btn-run-animation").innerText = "Restart animation";
-	const distanceTraveled = transAxisToCanvas({
-		dx: axDistTraveled({ fracOfC: USER_INFO.trainSpeed }),
-	}).dx;
+	subcanvases.each(canvasInfo => {
+		const distanceTrainTravels = transAxisToCanvas(canvasInfo, {
+			dx: axDistTraveled({ fracOfC: USER_INFO.trainSpeed }),
+		}).dx;
 
-	trackLines.ties
-		.transition()
-		.duration(0)
-		.attr("x1", d => d.baseX)
-		.attr("x2", d => d.baseX)
-		.each(d => {
-			if (d.observerIsStandingOn == "train") {
+		const distanceLightTravels = transAxisToCanvas(canvasInfo, {
+			dx: axDistTraveled({ fracOfC: 1 }),
+		}).dx;
+
+		tracks
+			.filter(d => d.type == "railroadTie" && d.canvas.observerIsStandingOn == "train")
+			.each(function (d) {
+				d3.select(this)
+					.transition()
+					.duration(0)
+					.attr("x1", d => d.x0)
+					.attr("x2", d => d.x0)
+					.transition()
+					.duration(TOTAL_DURATION_MS)
+					.ease(d3.easeLinear)
+					.attr("x1", d.x0 - distanceTrainTravels)
+					.attr("x2", d.x0 - distanceTrainTravels);
+			});
+
+		trainObjs.each(function (d) {
+			if (d.type == "photon") {
+				let xf;
+				if (d.direction == "left") {
+					xf = d.x0 - distanceLightTravels;
+				} else {
+					xf = d.x0 + distanceLightTravels;
+				}
+				d3.select(this)
+					.transition()
+					.duration(0)
+					.attr("cx", d.x0)
+					.transition()
+					.duration(TOTAL_DURATION_MS)
+					.ease(d3.easeLinear)
+					.attr("cx", xf);
+			} else if (
+				d.canvas.observerIsStandingOn == "ground" &&
+				(d.type == "train" || d.type == "lightSource")
+			) {
+				let xAttr;
+				if (d.type == "train") {
+					xAttr = "x";
+				} else if (d.type == "lightSource") {
+					xAttr = "cx";
+				}
+
+				d3.select(this)
+					.transition()
+					.duration(0)
+					.attr(xAttr, d.x0)
+					.transition()
+					.duration(TOTAL_DURATION_MS)
+					.ease(d3.easeLinear)
+					.attr(xAttr, d.x0 + distanceTrainTravels);
 			}
 		});
-
-	trackLines.ties
-		.transition()
-		.duration(0)
-		.attr("x1", d => d.baseX)
-		.attr("x2", d => d.baseX)
-		.transition()
-		.duration(TOTAL_DURATION_MS)
-		.ease(d3.easeLinear)
-		.attr("x1", d => d.baseX - distanceTraveled)
-		.attr("x2", d => d.baseX - distanceTraveled);
-
-	photons
-		.transition()
-		.duration(0)
-		.attr("cx", d => d.x0)
-		.transition()
-		.duration(TOTAL_DURATION_MS)
-		.ease(d3.easeLinear)
-		.attr("cx", d => d.x1);
+	});
 }
