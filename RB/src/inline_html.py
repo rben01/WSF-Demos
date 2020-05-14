83934df08491477f2996abb2382c35ed745e3afa
Robert Bennett <rltbennett@icloud.com>
2020-05-14 11:34:03 -0400

    Made inline_html more automatic, operating on the whole directory tree in one fell swoop

--- RB/src/inline_html.py
+++ RB/src/inline_html.py
@@ -6,30 +6,14 @@ from pathlib import Path
 from bs4 import BeautifulSoup, Tag
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("infile", help="The HTML file to inline")
-    parser.add_argument("-o", required=False, dest="outfile", help="Output filename")
-    parser.add_argument("--ignore", action="append", default=[])
-    parser.add_argument("--ignore-regex", action="append", default=[])
-
-    args = parser.parse_args()
-
-    infile = Path(args.infile)
-
-    if args.outfile is None:
-        outfile = infile.with_name(infile.stem + "_inlined").with_suffix(infile.suffix)
-    else:
-        outfile = Path(args.outfile)
-
+def inline(
+    infile: Path, outfile: Path, *, ignored_link_dests, ignored_link_dest_regexes
+):
     if infile.resolve() == outfile.resolve():
         sys.exit("infile and outfile are the same; they must be different")
 
     with infile.open() as f:
         soup = BeautifulSoup(f.read(), "lxml")
-
-    ignored_link_dests = set(args.ignore)
-    ignored_link_dest_regexes = [re.compile(regex) for regex in args.ignore_regex]
 
     tag_names_and_link_attrs = [("script", "src"), ("link", "href")]
 
@@ -61,7 +45,62 @@ def main():
                 soup.find("body").insert(0, style_tag)
 
     with outfile.open("w") as f:
-        f.write(soup.prettify())
+        f.write("\n".join(line.strip() for line in soup.prettify().splitlines()))
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("infile", nargs="?", help="The HTML file to inline")
+    parser.add_argument("-o", required=False, dest="outfile", help="Output filename")
+    parser.add_argument(
+        "--exclude-html-regex",
+        required=False,
+        help="A regex describing HTML files to skip; ignored if infile is given",
+    )
+    parser.add_argument(
+        "--ignore",
+        action="append",
+        default=[],
+        help="External resources to skip inlining",
+    )
+    parser.add_argument(
+        "--ignore-regex",
+        action="append",
+        default=[],
+        help="A regex describing external resources to skip inlining",
+    )
+
+    args = parser.parse_args()
+
+    infile_was_specified = args.infile is not None
+    if infile_was_specified:
+        infiles = [Path(args.infile)]
+    else:
+        if args.outfile is not None:
+            sys.exit("Cannot specify outfile without specifying an infile")
+
+        infiles = Path().glob("**/*.html")
+        if args.exclude_html_regex is not None:
+            ehr = re.compile(args.exclude_html_regex)
+            infiles = [p for p in infiles if not ehr.search(p.name)]
+
+    for infile in infiles:
+        if args.outfile is None:
+            outfile = infile.with_name(infile.stem + "_inlined").with_suffix(
+                infile.suffix
+            )
+        else:
+            outfile = Path(args.outfile)
+
+        ignored_link_dests = set(args.ignore)
+        ignored_link_dest_regexes = [re.compile(regex) for regex in args.ignore_regex]
+
+        inline(
+            infile,
+            outfile,
+            ignored_link_dests=ignored_link_dests,
+            ignored_link_dest_regexes=ignored_link_dest_regexes,
+        )
 
 
 if __name__ == "__main__":
