4d658643f6cbc28310bb60aed4096bfcb37d65be
Robert Bennett <rltbennett@icloud.com>
2020-05-13 22:47:13 -0400

    Made inline_js.py also inline css

--- RB/src/inline_js.py
+++ RB/src/inline_js.py
@@ -26,24 +26,37 @@ def main():
     with infile.open() as f:
         soup = BeautifulSoup(f.read(), "lxml")
 
-    ignored_srcs = set(args.ignore)
-    ignored_src_regexes = [re.compile(regex) for regex in args.ignore_regex]
+    ignored_link_dests = set(args.ignore)
+    ignored_link_dest_regexes = [re.compile(regex) for regex in args.ignore_regex]
 
-    scripts = soup.find_all("script")
-    for script in scripts:
-        script: Tag
-        src = script.attrs.get("src")
-        if (
-            src is None
-            or src in ignored_srcs
-            or any(regex.search(src) for regex in ignored_src_regexes)
-        ):
-            continue
+    tag_names_and_link_attrs = [("script", "src"), ("link", "href")]
 
-        script.attrs.pop("src")
-        absolute_src: Path = infile.parent / src
-        with absolute_src.open() as f:
-            script.string = f.read()
+    for tag_name, link_attr in tag_names_and_link_attrs:
+        tags = soup.find_all(tag_name)
+        for tag in tags:
+            tag: Tag
+            link_dest = tag.attrs.get(link_attr)
+            if (
+                link_dest is None
+                or link_dest in ignored_link_dests
+                or any(regex.search(link_dest) for regex in ignored_link_dest_regexes)
+            ):
+                continue
+
+            tag.attrs.pop(link_attr)
+            absolute_src: Path = infile.parent / link_dest
+
+            with absolute_src.open() as f:
+                contents = f.read()
+
+            if tag_name == "script":
+                tag.string = contents
+            elif tag_name == "link":
+                tag.decompose()
+                style_tag: Tag = soup.new_tag("style")
+                style_tag.attrs["type"] = "text/css"
+                style_tag.string = contents
+                soup.find("body").insert(0, style_tag)
 
     with outfile.open("w") as f:
         f.write(soup.prettify())
