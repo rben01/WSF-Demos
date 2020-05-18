dc41e9aecac9b5175281445ec951ccb721263de9
Robert Bennett <rltbennett@icloud.com>
2020-05-18 00:53:07 -0400

    Added further minification by compressing global symbols and properties

--- RB/src/inline_html.py
+++ RB/src/inline_html.py
@@ -1,4 +1,5 @@
 import argparse
+import enum
 import re
 import sys
 from pathlib import Path
@@ -6,9 +7,178 @@ from pathlib import Path
 import requests
 from bs4 import BeautifulSoup, Tag
 
+SYMBOLS_TO_REPLACE = [
+    "AX_HEIGHT",
+    "AX_MAX_X",
+    "AX_MAX_Y",
+    "AX_MID_X",
+    "AX_MID_Y",
+    "AX_MIN_X",
+    "AX_MIN_Y",
+    "AX_TRAIN_HEIGHT",
+    "AX_TRAIN_WIDTH",
+    "AX_WIDTH",
+    "CANVAS_HEIGHT",
+    "CANVAS_WIDTH",
+    "CONFIG",
+    "THIS_DEMO_SPEED_MULTIPLIER",
+    "TOTAL_DURATION_MS",
+    "TOTAL_DURATION_SEC",
+    "TRAIN_HEIGHT_PROPTN",
+    "TRAIN_WIDTH_PROPTN",
+    "USER_INFO",
+    "X_MARGIN_PROPTN",
+    "Y_MARGIN_PROPTN",
+    "addTracks",
+    "addTrainAndLightSources",
+    "animationEndDate",
+    "animationIsPlaying",
+    "animationStartDate",
+    "animationTimer",
+    "axDistBtwnTies",
+    "axDistTraveled",
+    "axDistTraveledAsFracOfTrainWidth",
+    "beginButton",
+    "durationMSOfAnimationReset",
+    "easingForAnimationReset",
+    "fracOfC",
+    "getRailroadTieParams",
+    "initialTieAxX",
+    "maxTrainSpeed",
+    "nTiesVisible",
+    "photons",
+    "playbackInfo",
+    "railroadRail",
+    "railroadTie",
+    "resetButton",
+    "trackInteriorAxMaxX",
+    "trackInteriorAxMinX",
+    "trackLines",
+    "trainCar",
+    "trainHeightProptn",
+    "trainLightSource",
+    "trainPhoton",
+    "trainSpeed",
+    "trainWidthProptn",
+    "transAxisToCanvas",
+    "xMarginProptn",
+    "yMarginProptn",
+]
+
+
+SYMBOL_RE: re.Pattern = re.compile(r"""(?<!")\b[a-zA-Z_]\w*\b(?!")""")
+
+
+class Lang(enum.Enum):
+    js = "javascript"
+
+
+class SymbolReplacer:
+    KEYWORDS = {
+        Lang.js: [
+            "abstract",
+            "arguments",
+            "boolean",
+            "break",
+            "byte",
+            "case",
+            "catch",
+            "char",
+            "const",
+            "continue",
+            "debugger",
+            "default",
+            "delete",
+            "do",
+            "double",
+            "else",
+            "eval",
+            "false",
+            "final",
+            "finally",
+            "float",
+            "for",
+            "function",
+            "goto",
+            "if",
+            "implements",
+            "in",
+            "instanceof",
+            "int",
+            "interface",
+            "let",
+            "long",
+            "native",
+            "new",
+            "null",
+            "package",
+            "private",
+            "protected",
+            "public",
+            "return",
+            "short",
+            "static",
+            "switch",
+            "synchronized",
+            "this",
+            "throw",
+            "throws",
+            "transient",
+            "true",
+            "try",
+            "typeof",
+            "var",
+            "void",
+            "volatile",
+            "while",
+            "with",
+            "yield",
+            "class",
+            "enum",
+            "export",
+            "extends",
+            "import",
+            "super",
+        ]
+    }
+
+    def __init__(self, base_symbols, lang=None):
+        self.symbols_used = set(sorted(s.lower() for s in base_symbols))
+        if lang is not None:
+            self.symbols_used.update(self.KEYWORDS[lang])
+
+        self.symbol_chars = ["a"]
+
+    def _advance(self):
+        for i, c in enumerate(self.symbol_chars):
+            if c == "z":
+                self.symbol_chars[i] = "a"
+            else:
+                self.symbol_chars[i] = chr(ord(c) + 1)
+                break
+        else:
+            self.symbol_chars.append("a")
+
+    def _current_symbol(self):
+        return "".join(reversed(self.symbol_chars)).lower()
+
+    def get_next_symbol(self) -> str:
+        symbol = self._current_symbol()
+        while symbol in self.symbols_used:
+            self._advance()
+            symbol = self._current_symbol()
+
+        self.symbols_used.add(symbol)
+        return symbol
+
 
 def inline(
-    infile: Path, outfile: Path, *, ignored_link_dests, ignored_link_dest_regexes
+    infile: Path,
+    outfile: Path,
+    *,
+    ignored_link_dests,
+    ignored_link_dest_regexes,
+    minify_globals=True
 ):
     if infile.resolve() == outfile.resolve():
         sys.exit("infile and outfile are the same; they must be different")
@@ -54,7 +224,19 @@ def inline(
         )
         script_tag = soup.new_tag("script")
         script_tag["type"] = "text/javascript"
-        script_tag.string = response.text
+        js_code = response.text
+
+        if minify_globals:
+            symbols = SYMBOL_RE.findall(js_code)
+            replacer = SymbolReplacer(symbols, lang=Lang.js)
+            for sym_to_replace in SYMBOLS_TO_REPLACE:
+                js_code = re.sub(
+                    r"""(?<!")\b{}\b(?!")""".format(sym_to_replace),
+                    replacer.get_next_symbol(),
+                    js_code,
+                )
+
+        script_tag.string = js_code
         soup.find("body").append(script_tag)
 
     if css_source_strs:
@@ -91,6 +273,11 @@ def main():
         default=[],
         help="A regex describing external resources to skip inlining",
     )
+    parser.add_argument(
+        "--minify-globals",
+        action="store_true",
+        help="Minify global variables and properties in JS",
+    )
 
     args = parser.parse_args()
 
@@ -107,9 +294,15 @@ def main():
 
     for infile in infiles:
         if args.outfile is None:
-            outfile = infile.with_name(infile.stem + "_inlined").with_suffix(
+            if args.minify_globals:
+                filename_suffix = "_inlined_min"
+            else:
+                filename_suffix = "_inlined"
+
+            outfile = infile.with_name(infile.stem + filename_suffix).with_suffix(
                 infile.suffix
             )
+
         else:
             outfile = Path(args.outfile)
 
@@ -121,6 +314,7 @@ def main():
             outfile,
             ignored_link_dests=ignored_link_dests,
             ignored_link_dest_regexes=ignored_link_dest_regexes,
+            minify_globals=args.minify_globals,
         )
 
 
