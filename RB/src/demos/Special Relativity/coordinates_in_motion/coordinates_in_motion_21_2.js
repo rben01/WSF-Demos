9863605862ac1fc1c4dd8a18cfad4db617700031
Robert Bennett <rltbennett@icloud.com>
2020-06-11 11:39:31 -0400

    Basic coordinates in motion demo, half finished

--- RB/src/demos/Special Relativity/coordinates_in_motion/coordinates_in_motion_21_2.js
+++ RB/src/demos/Special Relativity/coordinates_in_motion/coordinates_in_motion_21_2.js
@@ -1,20 +1,25 @@
-/* global l2Norm applyDatum applyGraphicalObjs */
+/* global C lorentzFactor applyDatum applyGraphicalObjs */
 "use strict";
 
 const RANGES = {
-	x: { min: -15, max: 15 },
-	y: { min: -1, max: 1 },
+	x: { min: -11, max: 11 },
+	y: { min: 0, max: 1 },
 };
+
+Object.values(RANGES).forEach(range => {
+	range.mid = (range.max + range.min) / 2;
+	range.span = range.max - range.min;
+});
 
 const speedSlider = document.getElementById("input-speed");
 
 const AXIS_MARGINS = {
 	top: 0.05,
-	bottom: 0.05,
+	bottom: 0.1,
 	left: 0.05,
 	right: 0.05,
 };
-const CANVAS_HEIGHT = 300;
+const CANVAS_HEIGHT = 200;
 const CANVAS_WIDTH = 800;
 const AX_BOUNDS = {
 	xMin: CANVAS_WIDTH * AXIS_MARGINS.left,
@@ -30,8 +35,8 @@ const textSpans = {
 
 const highlightFGColor = "#5df";
 
-const canvas = d3
-	.select("#viz-canvas")
+const canvases = d3
+	.selectAll(".viz-canvas")
 	.attr("width", CANVAS_WIDTH)
 	.attr("height", CANVAS_HEIGHT)
 	.attr("background-color", "black");
@@ -50,479 +55,220 @@ const ARROWHEAD_ID = "marker-arrowhead";
 		},
 	};
 	const pathAttrs = { attrs: { d: "M 0 0 L 10 4 L 0 8 L 4 4 z" } };
-	const defs = canvas.append("svg:defs");
+	const defs = canvases.append("svg:defs");
 
-	[
-		[ARROWHEAD_IDS.upright, "white"],
-		[ARROWHEAD_IDS.transformed, highlightFGColor],
-	].forEach(([id, color]) => {
-		defs.append("svg:marker")
-			.attr("id", id)
-			.each(function () {
-				applyDatum.call(this, markerAttrs);
-			})
-			.append("svg:path")
-			.each(function () {
-				applyDatum.call(this, pathAttrs);
-			})
-			.attr("stroke", color)
-			.attr("fill", color);
-	});
+	defs.append("svg:marker")
+		.attr("id", ARROWHEAD_ID)
+		.each(function () {
+			applyDatum.call(this, markerAttrs);
+		})
+		.append("svg:path")
+		.each(function () {
+			applyDatum.call(this, pathAttrs);
+		})
+		.attr("stroke", highlightFGColor)
+		.attr("fill", highlightFGColor);
 })();
 
-const subcanvases = canvas
-	.data([
+const subcanvases = canvases
+	.selectAll("g")
+	.data((_, index) => [
 		{
 			originX: 0,
 			originY: 0,
 			width: CANVAS_WIDTH,
 			height: CANVAS_HEIGHT,
+			isTopFrame: index === 0,
 		},
 	])
-	.each(function (d) {
-		d3.select(this)
-			.append("g")
-			.attr("transform", `translate(0, ${d.originY})`)
-			.classed("subcanvas", true);
-	})
-	.selectAll("g");
+	.join("g")
+	.attr("transform", d => `translate(0, ${d.originY})`)
+	.classed("subcanvas", true);
 
 const xScale = d3
 	.scaleLinear()
-	.domain([RANGES.axis.min, RANGES.axis.max])
+	.domain([RANGES.x.min, RANGES.x.max])
 	.range([AX_BOUNDS.xMin, AX_BOUNDS.xMax]);
 const yScale = d3
 	.scaleLinear()
-	.domain([RANGES.axis.min, RANGES.axis.max])
+	.domain([RANGES.y.min, RANGES.y.max])
 	.range([AX_BOUNDS.yMin, AX_BOUNDS.yMax]);
+const widthScale = d3
+	.scaleLinear()
+	.domain([0, RANGES.x.span])
+	.range([0, AX_BOUNDS.xMax - AX_BOUNDS.xMin]);
+const heightScale = d3
+	.scaleLinear()
+	.domain([0, RANGES.y.span])
+	.range([0, AX_BOUNDS.yMin - AX_BOUNDS.yMax]);
 
-function getAngleFromIndex(angleIndex) {
-	if (typeof angleIndex === "undefined") {
-		if (sliders.angle === null) {
-			return 0;
-		}
-		angleIndex = +sliders.angle.value;
-	}
-
-	return -(
-		RANGES.angle.min +
-		(angleIndex / (RANGES.angle.n - 1)) * (RANGES.angle.max - RANGES.angle.min)
-	);
+function getTimeDelta({ dx, fracOfC }) {
+	return (lorentzFactor({ fracOfC }) * dx * fracOfC) / 10;
 }
 
-const AXIS_LABEL_LOCS = {
-	xAx: { x: RANGES.axis.max, y: RANGES.axis.mid + RANGES.axis.span * 0.05 },
-	yAx: { x: RANGES.axis.mid + RANGES.axis.span * 0.05, y: RANGES.axis.max },
-};
-
-function drawGraph() {
-	subcanvases.each(function () {
-		const subcanvas = d3.select(this);
-		const axisAttrs = {
-			stroke: "white",
-			"stroke-width": 2,
-			"marker-end": `url(#${ARROWHEAD_IDS.upright})`,
-		};
-		subcanvas
-			.selectAll()
-			.data([
-				{
-					shape: "line",
-					class: "axis-x",
-					attrs: {
-						x1: xScale(RANGES.axis.min),
-						x2: xScale(RANGES.axis.max),
-						y1: yScale(RANGES.axis.mid),
-						y2: yScale(RANGES.axis.mid),
-						...axisAttrs,
-					},
-				},
-				{
-					shape: "line",
-					class: "axis-y",
-					attrs: {
-						x1: xScale(RANGES.axis.mid),
-						x2: xScale(RANGES.axis.mid),
-						y1: yScale(RANGES.axis.min),
-						y2: yScale(RANGES.axis.max),
-						...axisAttrs,
-					},
-				},
-			])
-			.enter()
-			.append("svg:line")
-			.each(applyDatum);
-
-		subcanvas
-			.selectAll("text")
-			.data([
-				{
-					shape: "text",
-					class: "axis-x-label",
-					attrs: {
-						x: xScale(AXIS_LABEL_LOCS.xAx.x),
-						y: yScale(AXIS_LABEL_LOCS.xAx.y),
-						fill: "white",
-						"text-anchor": "middle",
-						"dominant-baseline": "center",
-					},
-					text: "𝑥",
-				},
-				{
-					shape: "text",
-					class: "axis-y-label",
-					attrs: {
-						x: xScale(AXIS_LABEL_LOCS.yAx.x),
-						y: yScale(AXIS_LABEL_LOCS.yAx.y),
-						fill: "white",
-						"text-anchor": "middle",
-						"dominant-baseline": "center",
-					},
-					text: "𝑦",
-				},
-			])
-			.enter()
-			.append("svg:text")
-			.each(applyDatum)
-			.text(d => d.text);
-	});
+function getTextClassName(frameName) {
+	return `text-frame-${frameName}`;
 }
 
-function getLinesData({
-	x,
-	y,
-	angleRad,
-	displacementX,
-	displacementY,
-	shouldDrawUprightLines,
-}) {
-	if (typeof x === "undefined") {
-		x = +sliders.x.value;
-	}
+function getGraphData(d) {
+	const y0s = yScale(0);
 
-	if (typeof y === "undefined") {
-		y = +sliders.y.value;
-	}
+	function getBoxesAndTextData() {
+		const lineColor = "white";
+		const fillColor = "brown";
+		const boxHeightAboveGround = 0.2;
 
-	if (typeof angleRad === "undefined") {
-		if (sliders.angle === null) {
-			angleRad = 0;
-		} else {
-			angleRad = +getAngleFromIndex(sliders.angle.value);
-		}
-	}
+		const boxData = [];
+		const lineData = [];
+		const textData = [];
 
-	if (typeof displacementX === "undefined") {
-		if (sliders.displacement.x === null) {
-			displacementX = 0;
-		} else {
-			displacementX = sliders.displacement.x.value;
-		}
-	}
-	displacementX = +displacementX;
+		const boxXLocs = xScale.ticks(RANGES.x.span / 2);
+		const boxWidth = 1.8;
+		const boxHeight = 0.2;
+		boxXLocs.forEach(x => {
+			boxData.push({
+				shape: "rect",
+				class: "box",
+				attrs: {
+					x: xScale(x - boxWidth / 2),
+					y: yScale(boxHeightAboveGround + boxHeight),
+					width: widthScale(boxWidth),
+					height: heightScale(boxHeight),
+					stroke: lineColor,
+					"stroke-width": 2,
+					fill: fillColor,
+				},
+			});
 
-	if (typeof displacementY === "undefined") {
-		if (sliders.displacement.y === null) {
-			displacementY = 0;
-		} else {
-			displacementY = +sliders.displacement.y.value;
-		}
-	}
-	displacementY = +displacementY;
+			const xs = xScale(x);
 
-	if (typeof shouldDrawUprightLines === "undefined") {
-		shouldDrawUprightLines = true;
-	}
+			lineData.push({
+				shape: "line",
+				class: "box-line",
+				attrs: {
+					x1: xs,
+					y1: y0s,
+					x2: xs,
+					y2: yScale(boxHeightAboveGround),
+					stroke: lineColor,
+					"stroke-width": 2,
+				},
+			});
 
-	const sin = Math.sin(angleRad);
-	const cos = Math.cos(angleRad);
-
-	const tfAxesAttrs = {
-		"stroke-width": 2,
-		"marker-end": `url(#${ARROWHEAD_IDS.transformed})`,
-	};
-
-	const tfAxOrigin = {
-		x: RANGES.axis.mid + displacementX,
-		y: RANGES.axis.mid + displacementY,
-	};
-
-	const tfAxPosEndDisplacement = {
-		xAx: {
-			x: RANGES.axis.posDist * cos,
-			y: RANGES.axis.posDist * sin,
-		},
-		yAx: {
-			x: -RANGES.axis.posDist * sin,
-			y: RANGES.axis.posDist * cos,
-		},
-	};
-
-	// Looks like {xAx: {x: y: }, {yAx: {x: y: }}}
-	const tfAxPosEndCoords = {};
-	Object.entries(tfAxPosEndDisplacement).forEach(([ax, info]) => {
-		tfAxPosEndCoords[ax] = {};
-		Object.entries(info).forEach(([coord, dist]) => {
-			tfAxPosEndCoords[ax][coord] = tfAxOrigin[coord] + dist;
+			const frameName = d.isTopFrame ? "a" : "b";
+			textData.push({
+				shape: "text",
+				class: getTextClassName(frameName),
+				text: "10:00",
+				x: x,
+				attrs: {
+					x: xs,
+					y: yScale(boxHeightAboveGround + boxHeight / 2),
+					"font-size": 18,
+					fill: "#5f5",
+					"text-anchor": "middle",
+					"alignment-baseline": "middle",
+				},
+			});
 		});
-	});
 
-	const tfAxesData = [
-		{
-			shape: "line",
-			class: "tf-axis-x",
-			attrs: {
-				x1: xScale(tfAxOrigin.x - RANGES.axis.negDist * cos),
-				y1: yScale(tfAxOrigin.y - RANGES.axis.negDist * sin),
-				x2: xScale(tfAxPosEndCoords.xAx.x),
-				y2: yScale(tfAxPosEndCoords.xAx.y),
-				...tfAxesAttrs,
-				stroke: highlightFGColor,
-			},
-		},
-		{
-			shape: "line",
-			class: "tf-axis-y",
-			attrs: {
-				x1: xScale(tfAxOrigin.x + RANGES.axis.negDist * sin),
-				y1: yScale(tfAxOrigin.y - RANGES.axis.negDist * cos),
-				x2: xScale(tfAxPosEndCoords.yAx.x),
-				y2: yScale(tfAxPosEndCoords.yAx.y),
-				...tfAxesAttrs,
-				stroke: highlightFGColor,
-			},
-		},
-	];
-
-	const uprightLinesAttrs = {
-		stroke: "white",
-		"stroke-width": 2,
-		"stroke-dasharray": "5 5",
-	};
-	const uprightLinesData = !shouldDrawUprightLines
-		? []
-		: [
-				{
-					shape: "line",
-					class: "upright-line-horizontal",
-					attrs: {
-						x1: xScale(RANGES.axis.mid),
-						y1: yScale(y),
-						x2: xScale(x),
-						y2: yScale(y),
-						...uprightLinesAttrs,
-					},
-				},
-				{
-					shape: "line",
-					class: "upright-line-vertical",
-					attrs: {
-						x1: xScale(x),
-						y1: yScale(RANGES.axis.mid),
-						x2: xScale(x),
-						y2: yScale(y),
-						...uprightLinesAttrs,
-					},
-				},
-		  ];
-
-	// Compute projection of point onto transformed axes by translating point and transformed axes back to origin, computing projection onto transformed axes (now rotated but not translated), and then translating back
-	const tfAxProjCoords = {};
-	Object.keys(tfAxPosEndCoords).forEach(ax => {
-		const tfAxPosEndDispX = tfAxPosEndDisplacement[ax].x;
-		const tfAxPosEndDispY = tfAxPosEndDisplacement[ax].y;
-		const posEndNorm = l2Norm(tfAxPosEndDispX, tfAxPosEndDispY);
-		const posEndUnitVecX = tfAxPosEndDispX / posEndNorm;
-		const posEndUnitVecY = tfAxPosEndDispY / posEndNorm;
-		const projectionDotProd =
-			(x - tfAxOrigin.x) * posEndUnitVecX + (y - tfAxOrigin.y) * posEndUnitVecY;
-
-		tfAxProjCoords[ax] = {};
-		tfAxProjCoords[ax].x = tfAxOrigin.x + projectionDotProd * posEndUnitVecX;
-		tfAxProjCoords[ax].y = tfAxOrigin.y + projectionDotProd * posEndUnitVecY;
-		tfAxProjCoords[ax].dotProd = projectionDotProd;
-	});
-
-	const tfLinesAttrs = {
-		stroke: highlightFGColor,
-		"stroke-width": 2,
-		"stroke-dasharray": "5 5",
-	};
-	const tfLinesData = [
-		{
-			shape: "line",
-			class: "tf-line-vertical",
-			attrs: {
-				x1: xScale(tfAxProjCoords.xAx.x),
-				y1: yScale(tfAxProjCoords.xAx.y),
-				x2: xScale(x),
-				y2: yScale(y),
-				...tfLinesAttrs,
-			},
-		},
-		{
-			shape: "line",
-			class: "tf-line-horizontal",
-			attrs: {
-				x1: xScale(tfAxProjCoords.yAx.x),
-				y1: yScale(tfAxProjCoords.yAx.y),
-				x2: xScale(x),
-				y2: yScale(y),
-				...tfLinesAttrs,
-			},
-		},
-	];
-
-	const axisLabelDist = 0.05 * RANGES.axis.span;
-	const axisLabelAttrs = {
-		fill: highlightFGColor,
-	};
-	const getOpacity = (x, y) => {
-		const minDistToUprightAxisLabel = Math.min(
-			...["xAx", "yAx"].map(ax => {
-				console.log(x, y, ax, AXIS_LABEL_LOCS[ax]);
-				const distFromUprightAxisLabel = l2Norm(
-					x - AXIS_LABEL_LOCS[ax].x,
-					y - AXIS_LABEL_LOCS[ax].y,
-				);
-				return distFromUprightAxisLabel / RANGES.axis.span;
-			}),
-		);
-
-		const beginAppearScaledDist = 0.05;
-		const fullyAppearScaledDist = 0.07;
-		const unadjustedOpacity =
-			(1 / (fullyAppearScaledDist - beginAppearScaledDist)) *
-			(minDistToUprightAxisLabel - beginAppearScaledDist);
-		const opacity = Math.max(0, Math.min(unadjustedOpacity, 1));
-		return opacity;
-	};
-	const tfAxisLabelsData = [
-		(() => {
-			// X axis label
-			const x = tfAxPosEndCoords.xAx.x + axisLabelDist * -sin;
-			const y = (() => {
-				const y = tfAxPosEndCoords.xAx.y + axisLabelDist * cos;
-				if (cos < 0) {
-					return y + axisLabelDist * cos * 0.9;
-				}
-				return y;
-			})();
-
-			const opacity = getOpacity(x, y);
-
-			return {
-				shape: "text",
-				class: "tf-x-axis-label",
-				attrs: {
-					x: xScale(x),
-					y: yScale(y),
-					opacity: opacity,
-					...axisLabelAttrs,
-					"text-anchor": "middle",
-					"dominant-baseline": "center",
-				},
-				text: "𝑥′",
-			};
-		})(),
-		(() => {
-			// Y axis label
-			const x = tfAxPosEndCoords.yAx.x + axisLabelDist * cos * 1.2;
-			const y = (() => {
-				const y = tfAxPosEndCoords.yAx.y + axisLabelDist * sin;
-				if (sin < 0) {
-					return y + axisLabelDist * sin * 0.9;
-				}
-				return y;
-			})();
-
-			const opacity = getOpacity(x, y);
-
-			return {
-				shape: "text",
-				class: "tf-y-axis-label",
-				attrs: {
-					x: xScale(x),
-					y: yScale(y),
-					opacity: opacity,
-					...axisLabelAttrs,
-					"text-anchor": "middle",
-					"dominant-baseline": "center",
-				},
-				text: "𝑦′",
-			};
-		})(),
-	];
-
-	const pointDatum = {
-		shape: "circle",
-		class: "marked-point",
-		attrs: {
-			cx: xScale(x),
-			cy: yScale(y),
-			r: 4,
-			fill: "#ed0",
-		},
-	};
-
-	return {
-		data: [
-			...tfAxesData,
-			...uprightLinesData,
-			...tfLinesData,
-			...tfAxisLabelsData,
-			pointDatum,
-		],
-		tfCoords: {
-			xAxXCoord: tfAxProjCoords.xAx.dotProd,
-			yAxYCoord: tfAxProjCoords.yAx.dotProd,
-		},
-	};
-}
-
-function updateAngle({
-	x,
-	y,
-	angleIndex,
-	displacementX,
-	displacementY,
-	userInput = true,
-	shouldDrawUprightLines,
-} = {}) {
-	const angleRad = getAngleFromIndex(angleIndex);
-
-	const { data, tfCoords: tfCoords } = getLinesData({
-		x,
-		y,
-		angleRad,
-		displacementX,
-		displacementY,
-		shouldDrawUprightLines,
-	});
-
-	applyGraphicalObjs(subcanvases, () => data, {
-		selector: ".graph-obj",
-		cssClass: "graph-obj",
-	});
-
-	textSpans.upright.x.textContent = (+sliders.x.value).toFixed(2);
-	textSpans.upright.y.textContent = (+sliders.y.value).toFixed(2);
-	textSpans.transformed.x.textContent = tfCoords.xAxXCoord.toFixed(2);
-	textSpans.transformed.y.textContent = tfCoords.yAxYCoord.toFixed(2);
-
-	if (!userInput || typeof angleRad !== "undefined") {
-		textSpans.angle.textContent = (angleIndex === (RANGES.angle.n - 1) / 2
-			? +0
-			: angleRad
-		).toFixed(2);
+		return [...boxData, ...lineData, ...textData];
 	}
+
+	function getAxesData() {
+		const axisColor = "#999";
+
+		const [minX, maxX] = xScale.domain().map(xScale);
+		const axisDatum = {
+			class: "x-axis",
+			shape: "line",
+			attrs: {
+				x1: minX,
+				y1: y0s,
+				x2: maxX,
+				y2: y0s,
+				stroke: axisColor,
+				"stroke-width": 1,
+			},
+		};
+
+		const ticksData = xScale.ticks(RANGES.x.span).map(x => {
+			const xs = xScale(x);
+			return {
+				class: "x-axis-tick",
+				shape: "line",
+				attrs: {
+					x1: xs,
+					y1: y0s,
+					x2: xs,
+					y2: yScale(0.03),
+					stroke: axisColor,
+					"stroke-width": 1,
+				},
+			};
+		});
+
+		const labelsData = xScale.ticks(5).map(x => {
+			const xs = xScale(x);
+			return {
+				class: "x-axis-label",
+				shape: "text",
+				text: `${x}`,
+				attrs: {
+					x: xs,
+					y: yScale(-AXIS_MARGINS.bottom / 2),
+					fill: axisColor,
+					"font-size": 12,
+					"text-anchor": "middle",
+					"alignment-baseline": "middle",
+				},
+			};
+		});
+
+		return [axisDatum, ...ticksData, ...labelsData];
+	}
+
+	return [...getBoxesAndTextData(), ...getAxesData()];
 }
 
-drawGraph();
-updateAngle({
-	angleRad: 0,
-	displacementX: 0,
-	displacementY: 0,
-	userInput: false,
-	shouldDrawUprightLines: sliders.angle !== null,
+function timeToStr(referenceMinute, secondsAfterReferenceMinute) {
+	secondsAfterReferenceMinute = Math.round(secondsAfterReferenceMinute);
+	const minutesAfterReferenceMinute = Math.floor(secondsAfterReferenceMinute / 60);
+
+	const second = secondsAfterReferenceMinute - minutesAfterReferenceMinute * 60;
+	const minute = referenceMinute + minutesAfterReferenceMinute;
+	console.log(minute, second, secondsAfterReferenceMinute);
+
+	const minuteStr = minute.toFixed(0).padStart(2, "0");
+	const secondStr = second.toFixed(0).padStart(2, "0");
+
+	return `${minuteStr}:${secondStr}`;
+}
+
+function updateClocks({ fracOfC, topFrameIsStationary }) {
+	const stationaryFrameName = getTextClassName(topFrameIsStationary ? "a" : "b");
+	const movingFrameName = getTextClassName(topFrameIsStationary ? "b" : "a");
+
+	const stationaryText = d3.selectAll(`.${stationaryFrameName}`);
+	const movingText = d3.selectAll(`.${movingFrameName}`);
+
+	stationaryText.each(function () {
+		const text = d3.select(this);
+		text.text(timeToStr(10, 0));
+	});
+
+	movingText.each(function (d) {
+		const x = d.x;
+		const dtSeconds = getTimeDelta({ dx: x, fracOfC: fracOfC });
+
+		const text = d3.select(this);
+		text.text(timeToStr(10, dtSeconds));
+	});
+}
+
+subcanvases.each(function (d) {
+	const subcanvas = d3.select(this);
+	applyGraphicalObjs(subcanvas, () => getGraphData(d));
 });
+
+updateClocks({ fracOfC: -0.9999, topFrameIsStationary: true });
