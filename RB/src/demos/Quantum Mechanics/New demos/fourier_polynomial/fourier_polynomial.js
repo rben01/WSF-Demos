edbbb8e088fccf1543f3cbd57e1366bacb1d1b50
Danny Swift <dswift333@gmail.com>
2020-12-01 16:18:29 -0500

    rearranging

--- RB/src/demos/Quantum Mechanics/New demos/fourier_polynomial/fourier_polynomial.js
+++ RB/src/demos/Quantum Mechanics/New demos/fourier_polynomial/fourier_polynomial.js
@@ -18,25 +18,27 @@ const sliders = {
     cubic: document.getElementById("cubic")
 }
 
-const poly_graph = d3.select("#svg").append("g").attr("transform", "translate(" + (25 + w / 2) + ", " + (h / 2 + 25) + ")");
-poly_graph.append("g").attr("class", "axis").call(d3.axisBottom(x).ticks(7));
-poly_graph.append("g").attr("class", "axis").call(d3.axisLeft(y).ticks(7));
-const poly = poly_graph.append("path").attr("fill", "none").style("stroke", "lightgrey").style("stroke-width", 3);
-
-const peri_graph = d3.select("#svg2").append("g").attr("transform", "translate(" + (25 + w / 2) + ", " + (h / 2 + 25) + ")");
-peri_graph.append("g").attr("class", "axis").call(d3.axisBottom(x).ticks(7));
-peri_graph.append("g").attr("class", "axis").call(d3.axisLeft(y).ticks(7));
-const peri = peri_graph.append("path").attr("fill", "none").style("stroke", "lightgrey").style("stroke-width", 3);
-
-const cos_graph = d3.select("#svg3").append("g").attr("transform", "translate(" + 25 + ", " + (h / 2 + 25) + ")");
+const cos_graph = d3.select("#svg").append("g").attr("transform", "translate(" + 25 + ", " + (h / 2 + 25) + ")");
 cos_graph.append("g").attr("class", "axis").call(d3.axisBottom(cx).ticks(0));
 cos_graph.append("g").attr("class", "axis").call(d3.axisLeft(cy).ticks(0));
 const cos = cos_graph.append("path").attr("fill", "none").style("stroke", "lightgrey").style("stroke-width", 3);
 
-const sin_graph = d3.select("#svg4").append("g").attr("transform", "translate(" + 25 + ", " + (h / 2 + 25) + ")");
+const sin_graph = d3.select("#svg2").append("g").attr("transform", "translate(" + 25 + ", " + (h / 2 + 25) + ")");
 sin_graph.append("g").attr("class", "axis").call(d3.axisBottom(cx).ticks(0));
 sin_graph.append("g").attr("class", "axis").call(d3.axisLeft(cy).ticks(0));
 const sin = sin_graph.append("path").attr("fill", "none").style("stroke", "lightgrey").style("stroke-width", 3);
+
+const poly_graph = d3.select("#svg3").append("g").attr("transform", "translate(" + (25 + w / 2) + ", " + (h / 2 + 25) + ")");
+poly_graph.append("g").attr("class", "axis").call(d3.axisBottom(x).ticks(7));
+poly_graph.append("g").attr("class", "axis").call(d3.axisLeft(y).ticks(7));
+const poly = poly_graph.append("path").attr("fill", "none").style("stroke", "lightgrey").style("stroke-width", 3);
+
+const peri_graph = d3.select("#svg4").append("g").attr("transform", "translate(" + (25 + w / 2) + ", " + (h / 2 + 25) + ")");
+peri_graph.append("g").attr("class", "axis").call(d3.axisBottom(x).ticks(7));
+peri_graph.append("g").attr("class", "axis").call(d3.axisLeft(y).ticks(7));
+const peri = peri_graph.append("path").attr("fill", "none").style("stroke", "lightgrey").style("stroke-width", 3);
+
+
 
 const fourier_graph = d3.select("#svg5").append("g").attr("transform", "translate(" + (25 + w / 2) + ", " + (h / 2 + 25) + ")");
 fourier_graph.append("g").attr("class", "axis").call(d3.axisBottom(x).ticks(7));
