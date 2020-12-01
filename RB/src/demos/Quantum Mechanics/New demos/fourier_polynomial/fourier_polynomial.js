9e5aad9ad6ac71b8bf0e04f185c464c0577a2f78
Danny Swift <dswift333@gmail.com>
2020-12-01 15:01:01 -0500

    globalized coefficients and added funcs to get sin and cos coefficients

--- RB/src/demos/Quantum Mechanics/New demos/fourier_polynomial/fourier_polynomial.js
+++ RB/src/demos/Quantum Mechanics/New demos/fourier_polynomial/fourier_polynomial.js
@@ -4,6 +4,11 @@ const svg = d3.select("#svg"),
     x = d3.scaleLinear().range([-w / 2, w / 2]).domain([-6, 6]),
     y = d3.scaleLinear().range([-h / 2, h / 2]).domain([-10, 10]);
 
+var c0 = 0,
+    c1 = 0,
+    c2 = 0,
+    c3 = 0;
+
 const sliders = {
     constant: document.getElementById("constant"),
     linear: document.getElementById("linear"),
@@ -22,13 +27,9 @@ peri_graph.append("g").attr("class", "axis").call(d3.axisLeft(y).ticks(7));
 const peri = peri_graph.append("path").attr("fill", "none").style("stroke", "lightgrey").style("stroke-width", 3);
 
 const cos_graph = svg.append("g").attr("transform", "translate(" + (100 + 5 * w / 2) + ", " + (h / 2 + 50) + ")");
-cos_graph.append("g").attr("class", "axis").call(d3.axisBottom(x).ticks(7));
-cos_graph.append("g").attr("class", "axis").call(d3.axisLeft(y).ticks(7));
 const cos = cos_graph.append("path").attr("fill", "none").style("stroke", "lightgrey").style("stroke-width", 3);
 
 const sin_graph = svg.append("g").attr("transform", "translate(" + (150 + 7 * w / 2) + ", " + (h / 2 + 50) + ")");
-sin_graph.append("g").attr("class", "axis").call(d3.axisBottom(x).ticks(7));
-sin_graph.append("g").attr("class", "axis").call(d3.axisLeft(y).ticks(7));
 const sin = sin_graph.append("path").attr("fill", "none").style("stroke", "lightgrey").style("stroke-width", 3);
 
 const fourier_graph = svg.append("g").attr("transform", "translate(" + (200 + 9 * w / 2) + ", " + (h / 2 + 50) + ")");
@@ -37,11 +38,11 @@ fourier_graph.append("g").attr("class", "axis").call(d3.axisLeft(y).ticks(7));
 const fourier = fourier_graph.append("path").attr("fill", "none").style("stroke", "lightgrey").style("stroke-width", 3);
 
 function get_poly_path() {
-    var c0 = +sliders.constant.value,
-        c1 = +sliders.linear.value,
-        c2 = +sliders.quadratic.value,
-        c3 = +sliders.cubic.value,
-        points = [];
+    var points = [];
+    c0 = +sliders.constant.value;
+    c1 = +sliders.linear.value;
+    c2 = +sliders.quadratic.value;
+    c3 = +sliders.cubic.value;
 
     for (var i = -6; i < 6; i += 12 / 1000) {
         points.push([w * i / 12, -h * (c3 * i ** 3 + c2 * i ** 2 + c1 * i + c0) / 20])
@@ -51,11 +52,7 @@ function get_poly_path() {
 }
 
 function get_peri_path() {
-    var c0 = +sliders.constant.value,
-        c1 = +sliders.linear.value,
-        c2 = +sliders.quadratic.value,
-        c3 = +sliders.cubic.value,
-        points = [],
+    var points = [],
         x;
 
     for (var i = -6; i < 6; i += 12 / 1000) {
@@ -64,6 +61,30 @@ function get_peri_path() {
     }
 
     return points;
+}
+
+/* Fourier series:
+Even functions - f(x) = a_0/2 + Sigma a_n cos(n pi x / L), a_n = 2/L int [0,L] f(x) cos(n pi x / L) dx
+Odd function - f(x) = Sigma b_n sin (n pi x / L), b_n = 2/L int [0,L] f(x) cos(n pi x / L) dx
+
+*/
+
+function get_sin_coefs() {
+    var coefs = [];
+    for (var n = 1; n < 11; n++) {
+        coefs.push(c1 * 2 * (-1) ** (n + 1) / n + c3 * 2 * (-1) ** (n + 1) * (Math.PI ** 2 - 6 / n ** 2) / n);
+    }
+
+    return coefs;
+}
+
+function get_cos_coefs() {
+    var coefs = [c0 + c2 * 4 * Math.PI ** 2 / 3];
+    for (var n = 1; n < 10; n++) {
+        coefs.push(c2 * 4 * (-1) ** n / n ** 2);
+    }
+
+    return coefs;
 }
 
 function update() {
