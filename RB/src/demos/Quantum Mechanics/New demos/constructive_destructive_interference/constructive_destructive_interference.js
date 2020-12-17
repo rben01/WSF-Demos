a0c338afb34eb48c6bac9c6d43213aa85bee855b
Robert Bennett <rltbennett@icloud.com>
2020-12-17 13:27:55 -0500

    Added photons and colors to lights

--- RB/src/demos/Quantum Mechanics/New demos/constructive_destructive_interference/constructive_destructive_interference.js
+++ RB/src/demos/Quantum Mechanics/New demos/constructive_destructive_interference/constructive_destructive_interference.js
@@ -4,7 +4,7 @@ const plotElem = document.getElementById("plot");
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
 	75,
-	window.innerWidth / window.innerHeight,
+	plotElem.clientWidth / plotElem.clientHeight,
 	0.1,
 	2000,
 );
@@ -33,10 +33,27 @@ const MAX_ANGLE_RAD = Math.atan2(DETECTOR_WIDTH * 0.495, DISTANCE);
 
 const CAMERA_DEFAULT_POSITION = new THREE.Vector3(60, 40, CAMERA_Z);
 const CAMERA_POINT_OF_FOCUS = new THREE.Vector3(0, -20, CAMERA_Z);
-const CAMERA_DIST_FROM_POINT_OF_FOCUS = CAMERA_DEFAULT_POSITION.distanceTo(
-	CAMERA_POINT_OF_FOCUS,
-);
 const DRAG_SPEED = 0.002; // scale factor to convert pixels dragged to radians
+
+// const slitSeparationTextElem = document.getElementById("slit-separation-text");
+// const slitSeparationTextPos = new THREE.Vector3(
+// 	0,
+// 	SOURCE_PANE_HEIGHT / 2 + 4,
+// 	SOURCE_Z + 0.01,
+// );
+// function updateSlitSeparationTextElemPos() {
+// 	const textPos = slitSeparationTextPos.clone().project(camera);
+// 	console.log(textPos);
+// 	const plotElemRect = plotElem.getBoundingClientRect();
+// 	const halfWidth = plotElemRect.width / 2;
+// 	const halfHeight = plotElemRect.height / 2;
+// 	const textPosX = plotElemRect.x + halfWidth + textPos.x * halfWidth;
+// 	const textPosY = plotElemRect.y + halfHeight - textPos.y * halfHeight;
+
+// 	slitSeparationTextElem.style.left = `${textPosX}px`;
+// 	slitSeparationTextElem.style.top = `${textPosY}px`;
+// }
+// updateSlitSeparationTextElemPos();
 
 d3.select(plotElem).call(
 	d3.drag().on("drag", function (event) {
@@ -68,6 +85,8 @@ d3.select(plotElem).call(
 		camera.lookAt(CAMERA_POINT_OF_FOCUS);
 		camera.updateProjectionMatrix();
 		renderer.render(scene, camera);
+
+		// updateSlitSeparationTextElemPos();
 	}),
 );
 
@@ -83,7 +102,7 @@ const DETECTOR_MATERIAL_BACK = new THREE.MeshPhongMaterial({
 	color: 0x99aabb,
 	side: THREE.FrontSide,
 	transparent: true,
-	opacity: 0.5,
+	opacity: 0.2,
 	reflectivity: 0.2,
 });
 
@@ -100,17 +119,15 @@ const OUTLINE_MATERIAL = new THREE.LineBasicMaterial({
 	linewidth: 1,
 });
 
-const WAVE_MATERIAL = new THREE.LineDashedMaterial({
-	color: 0xffffff,
-	linewidth: 1,
-	dashSize: 10,
-	gapSize: 20,
-});
-
 const DETECTOR_INTENSITY_MATERIAL = new THREE.MeshBasicMaterial({
 	color: 0xffffff,
 	side: THREE.DoubleSide,
 	// clippingPlanes: [new THREE.Plane(new THREE.Vector3(0, 0, 1), -DETECTOR_Z - 0.3)],
+});
+
+const PHOTON_MATERIAL = new THREE.MeshBasicMaterial({
+	color: 0xffffff,
+	side: THREE.DoubleSide,
 });
 
 function get3DVerticesForRectAtOrigin(width, height, z) {
@@ -193,6 +210,40 @@ function getIntensityPath({
 	return [pathFrom(topHalfPoints), pathFrom(bottomHalfPoints)];
 }
 
+function getPhotonPath({
+	originCx,
+	originDisplacement,
+	originCy,
+	angle,
+	wavelength,
+	amplitude,
+	nPoints,
+}) {
+	originCy = originCy ?? 0;
+	amplitude = amplitude ?? SOURCE_PANE_HEIGHT * 0.3;
+	nPoints = nPoints ?? 1000;
+
+	const endPointX = originCx + DISTANCE * Math.tan(angle);
+	const originX = originCx + originDisplacement;
+	const waveTotalLengthPx = (DISTANCE ** 2 + (originX - endPointX) ** 2) ** 0.5;
+	const waveTotalLengthMeters = screenUnitsToMetersScale(waveTotalLengthPx);
+	const nPeriodsTotal = waveTotalLengthMeters / wavelength;
+
+	const points = [];
+	for (let i = 0; i < nPoints; ++i) {
+		const t = i / (nPoints - 1);
+		const x = (1 - t) * originX + t * endPointX;
+
+		const phase = t * nPeriodsTotal;
+		const y = originCy + amplitude * Math.sin(2 * Math.PI * phase);
+
+		const z = (1 - t) * SOURCE_Z + t * DETECTOR_Z;
+		points.push(new THREE.Vector3(x, y, z));
+	}
+
+	return new THREE.CatmullRomCurve3(points);
+}
+
 const DEFAULT_ANGLE = 0;
 const DEFAULT_WAVELENGTH = 500e-9;
 const DEFAULT_SLIT_SEPARATION = 2;
@@ -202,6 +253,12 @@ const MAX_WAVELENGTH = 800e-9;
 
 const MIN_SLIT_SEPARATION = 2;
 const MAX_SLIT_SEPARATION = 30;
+
+const colorScale = t => d3.interpolateSinebow(t * 0.8);
+const wavelengthScale = d3.scaleLinear([MAX_WAVELENGTH, MIN_WAVELENGTH], [0, 1]);
+
+const ARROWHEAD_LENGTH = 1;
+const ARROWHEAD_WIDTH = 0.6;
 
 let currAngle = DEFAULT_ANGLE;
 let currWavelength = DEFAULT_WAVELENGTH;
@@ -246,13 +303,13 @@ function updateEnvironment({ angle, wavelength, slitSeparation, objects }) {
 			for (const paneObjsKey of ["leftPaneObjs", "rightPaneObjs"]) {
 				const pane = new THREE.Mesh(outerPaneGeometry, SOURCE_PANE_MATERIAL);
 				pane.position.z = SOURCE_Z;
-				scene.add(pane);
 
 				const border = new THREE.Line(
 					outerPlaneBorderGeometry,
 					OUTLINE_MATERIAL,
 				);
-				scene.add(border);
+
+				scene.add(pane, border);
 
 				objects[paneObjsKey] = { pane, border };
 			}
@@ -265,12 +322,37 @@ function updateEnvironment({ angle, wavelength, slitSeparation, objects }) {
 				SOURCE_PANE_MATERIAL,
 			);
 			pane.position.z = SOURCE_Z;
-			scene.add(pane);
 
 			const border = new THREE.Line(new THREE.BufferGeometry(), OUTLINE_MATERIAL);
 
-			scene.add(border);
-			objects.middlePaneObjs = { pane, border };
+			const arrowOrigin = new THREE.Vector3(
+				0,
+				SOURCE_PANE_HEIGHT / 2 + 1,
+				SOURCE_Z,
+			);
+
+			const leftArrowDir = new THREE.Vector3(-1, 0, 0);
+			leftArrowDir.normalize();
+			const rightArrowDir = new THREE.Vector3(1, 0, 0);
+			rightArrowDir.normalize();
+
+			function makeArrow(direction) {
+				return new THREE.ArrowHelper(
+					direction,
+					arrowOrigin,
+					0,
+					0xffffff,
+					ARROWHEAD_LENGTH,
+					ARROWHEAD_WIDTH,
+				);
+			}
+			const leftArrow = makeArrow(leftArrowDir);
+			const rightArrow = makeArrow(rightArrowDir);
+			const arrows = [leftArrow, rightArrow];
+
+			scene.add(pane, border, leftArrow, rightArrow);
+
+			objects.middlePaneObjs = { pane, border, arrows };
 		})();
 
 		// Detector
@@ -317,14 +399,14 @@ function updateEnvironment({ angle, wavelength, slitSeparation, objects }) {
 			objects.amplitudeObjs = { topCurve, bottomCurve };
 		})();
 
-		// Fringe lights shining on front of detector Initially add all lights and make
-		// them invisible, then toggle their visibility as appropriate (saves the cost
-		// of adding/removing them)
+		// Fringe lights shining on front of detector. It's actually more efficient to
+		// add every light and keep them all visible at all times than to try to be
+		// clever about toggling off lights that don't shine on anything visible
 		(() => {
 			const fringeObjs = {};
 			const fringeLights = [];
 			fringeObjs.fringeLights = fringeLights;
-			// 2 * maxLightIndex + 1 == max number of lights visible
+			// max number of lights visible == 2 * maxLightIndex + 1
 			const maxLightIndex = Math.ceil(
 				(screenUnitsToMetersScale(MAX_SLIT_SEPARATION) *
 					Math.sin(MAX_ANGLE_RAD)) /
@@ -337,6 +419,23 @@ function updateEnvironment({ angle, wavelength, slitSeparation, objects }) {
 			}
 
 			objects.fringeObjs = fringeObjs;
+		})();
+
+		// The photons traveling from the source to the detector
+		(() => {
+			const photonObjs = {};
+			const photons = [];
+			photonObjs.photons = photons;
+
+			const photonGeometry = new THREE.BufferGeometry();
+
+			const leftPhoton = new THREE.Mesh(photonGeometry, PHOTON_MATERIAL);
+			const rightPhoton = new THREE.Mesh(photonGeometry, PHOTON_MATERIAL);
+
+			scene.add(leftPhoton, rightPhoton);
+			photons.push(leftPhoton, rightPhoton);
+
+			objects.photonObjs = photonObjs;
 		})();
 	}
 
@@ -357,7 +456,7 @@ function updateEnvironment({ angle, wavelength, slitSeparation, objects }) {
 
 	// Middle pane
 	(() => {
-		const { pane, border } = objects.middlePaneObjs;
+		const { pane, border, arrows } = objects.middlePaneObjs;
 		pane.geometry.dispose();
 		const paneGeometry = new THREE.PlaneBufferGeometry(
 			slitSeparation,
@@ -371,6 +470,12 @@ function updateEnvironment({ angle, wavelength, slitSeparation, objects }) {
 		);
 		border.geometry.dispose();
 		border.geometry = borderGeometry;
+
+		for (const arrow of arrows) {
+			const arrowLength =
+				slitSeparation / 2 + SOURCE_PANE_GAP_WIDTH - ARROWHEAD_LENGTH;
+			arrow.setLength(arrowLength, ARROWHEAD_LENGTH, ARROWHEAD_WIDTH);
+		}
 	})();
 
 	//
@@ -404,6 +509,8 @@ function updateEnvironment({ angle, wavelength, slitSeparation, objects }) {
 		}
 	})();
 
+	const color = new THREE.Color(colorScale(wavelengthScale(wavelength)));
+
 	// Lights on front of detector
 	(() => {
 		const { fringeLights } = objects.fringeObjs;
@@ -420,8 +527,29 @@ function updateEnvironment({ angle, wavelength, slitSeparation, objects }) {
 			const theta = Math.asin((lightIndex * wavelength) / slitSeparationMeters);
 			const x = screenUnitsToMetersScale.invert(distanceMeters * Math.tan(theta));
 
+			light.color = color;
 			light.position.set(x, 0, DETECTOR_Z - 2);
 			light.lookAt(x, 0, 0);
+		}
+	})();
+
+	// Photons traveling from source to detector
+	(() => {
+		const { photonObjs } = objects;
+		const photons = photonObjs.photons;
+		const displacement = slitSeparation / 2 + SOURCE_PANE_GAP_WIDTH / 2;
+
+		for (let i = 0; i < photons.length; ++i) {
+			const photon = photons[i];
+			photon.geometry.dispose();
+			const photonPath = getPhotonPath({
+				originCx: 0,
+				originDisplacement: (i === 0 ? -1 : 1) * displacement,
+				angle,
+				wavelength,
+			});
+			photon.geometry = new THREE.TubeBufferGeometry(photonPath, 1000, 0.15, 10);
+			photon.material.color = color;
 		}
 	})();
 
@@ -440,8 +568,9 @@ const objs = updateEnvironment({});
 // Angle slider
 (() => {
 	const slider = document.getElementById("slider-angle");
-	slider.min = 0;
-	slider.max = MAX_ANGLE_RAD * 0.99;
+	const maxAngle = MAX_ANGLE_RAD * 0.99;
+	slider.min = -maxAngle;
+	slider.max = maxAngle;
 	slider.step = 0.001;
 	slider.value = DEFAULT_ANGLE;
 })();
