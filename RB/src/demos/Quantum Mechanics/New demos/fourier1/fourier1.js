cd928a772a1d7ef05093c5f0419b71f9ddae80d4
Danny Swift <dswift333@gmail.com>
2020-12-23 12:04:14 -0500

    fourier1 working

--- RB/src/demos/Quantum Mechanics/New demos/fourier1/fourier1.js
+++ RB/src/demos/Quantum Mechanics/New demos/fourier1/fourier1.js
@@ -1,24 +1,68 @@
 const W = 1350,
     H = 750,
     svg = d3.select("#svg"),
-    g1 = svg.append("g").attr("transform", `translate(50, 25)`),
-    g2 = svg.append("g").attr("transform", `translate(${W / 2 + 100}, 25)`),
+    g1 = svg.append("g").attr("transform", `translate(25, 25)`),
+    g2 = svg.append("g").attr("transform", `translate(${W / 2 + 50}, 25)`),
     x1 = d3.scaleLinear().domain([-3, 3]).range([0, W / 2 - 100]),
     y1 = d3.scaleLinear().domain([-4, 4]).range([H - 50, 0]),
     x2 = d3.scaleLinear().domain([0, 9]).range([0, W / 2 - 100]),
     y2 = d3.scaleLinear().domain([-2, 2]).range([H - 50, 0]),
-    xax1 = g1.append("g").attr("class", "axis").attr("transform", `translate(0, ${H - 50})`).call(d3.axisBottom(x1).ticks(6)),
-    yax1 = g1.append("g").attr("class", "axis").call(d3.axisLeft(y1).tickValues([0.5, 1.0, 1.5, 2.0])),
-    xax2 = g2.append("g").attr("class", "axis").attr("transform", `translate(0, ${H - 50})`).call(d3.axisBottom(x2).ticks(5)),
-    yax2 = g2.append("g").attr("class", "axis").attr("transform", `translate(${W / 4 - 50}, 0)`).call(d3.axisLeft(y2).tickValues([0.2, 0.4, 0.6, 0.8, 1.0])),
-    aline = g1.append("line").style("stroke", "white").attr("y1", H - 25).attr("y2", 0).attr("x1", x1(1)).attr("x2", x1(1)),
-    dline = g2.append("path").style("stroke", "#5df").style("fill", "#5df").style("fill-opacity", 0.5),
-    mode1 = document.getElementById("mode1"),
-    mode2 = document.getElementById("mode2"),
-    mode3 = document.getElementById("mode3"),
-    mode4 = document.getElementById("mode4"),
-    mode5 = document.getElementById("mode5"),
-    mode6 = document.getElementById("mode6"),
-    mode7 = document.getElementById("mode7"),
-    mode8 = document.getElementById("mode8"),
-    mode9 = document.getElementById("mode9");
+    step = g1.append("path").style("stroke", "white").style("stroke-width", 3).attr("d", `M 0,${y1(-1.5)} L ${x1(-1)},${y1(-1.5)} L ${x1(-1)},${y1(1.5)} L ${x1(1)},${y1(1.5)} L ${x1(1)},${y1(-1.5)} L ${x1(3)},${y1(-1.5)}`),
+    sumline = g1.append("path").style("stroke", "#5df").style("stroke-width", 3).style("fill", "none"),
+    ampline = g2.append("path").style("stroke", "#5df").style("stroke-width", 2).style("fill", "none").style("stroke-dasharray", "15 15"),
+    xax1 = g1.append("g").attr("class", "axis").attr("transform", `translate(0, ${H / 2 - 25})`).call(d3.axisBottom(x1).tickValues([-3, -2, -1, 1, 2, 3])),
+    yax1 = g1.append("g").attr("class", "axis").attr("transform", `translate(${W / 4 - 50})`).call(d3.axisLeft(y1).tickValues([-4, -2, 2, 4])),
+    xax2 = g2.append("g").attr("class", "axis").attr("transform", `translate(0, ${H / 2 - 25})`).call(d3.axisBottom(x2).tickValues([1, 2, 3, 4, 5, 6, 7, 8, 9])),
+    yax2 = g2.append("g").attr("class", "axis").call(d3.axisLeft(y2).tickValues([-2, -1, 1, 2])),
+    modes = [document.getElementById("mode1"),
+    document.getElementById("mode2"),
+    document.getElementById("mode3"),
+    document.getElementById("mode4"),
+    document.getElementById("mode5"),
+    document.getElementById("mode6"),
+    document.getElementById("mode7"),
+    document.getElementById("mode8"),
+    document.getElementById("mode9")],
+    mlines = [
+        g2.append("line").style("stroke", "white").attr("x1", x2(1)).attr("x2", x2(1)).attr("y1", H / 2 - 25),
+        g2.append("line").style("stroke", "white").attr("x1", x2(2)).attr("x2", x2(2)).attr("y1", H / 2 - 25),
+        g2.append("line").style("stroke", "white").attr("x1", x2(3)).attr("x2", x2(3)).attr("y1", H / 2 - 25),
+        g2.append("line").style("stroke", "white").attr("x1", x2(4)).attr("x2", x2(4)).attr("y1", H / 2 - 25),
+        g2.append("line").style("stroke", "white").attr("x1", x2(5)).attr("x2", x2(5)).attr("y1", H / 2 - 25),
+        g2.append("line").style("stroke", "white").attr("x1", x2(6)).attr("x2", x2(6)).attr("y1", H / 2 - 25),
+        g2.append("line").style("stroke", "white").attr("x1", x2(7)).attr("x2", x2(7)).attr("y1", H / 2 - 25),
+        g2.append("line").style("stroke", "white").attr("x1", x2(8)).attr("x2", x2(8)).attr("y1", H / 2 - 25),
+        g2.append("line").style("stroke", "white").attr("x1", x2(9)).attr("x2", x2(9)).attr("y1", H / 2 - 25)
+    ];
+
+var mode_vals;
+function update() {
+    mode_vals = [];
+    for (var i = 0; i < 9; i++) {
+        mode_vals.push(modes[i].value);
+        mlines[i].attr("y2", y2(modes[i].value));
+    }
+
+    var d = [];
+    var y;
+    for (var i = -3; i < 3; i += 0.01) {
+        y = 0;
+        for (var n = 0; n < 9; n++) {
+            y += mode_vals[n] * Math.cos(Math.PI * (n + 1) * i / 2);
+        }
+        d.push([x1(i), y1(y)]);
+    }
+    sumline.attr("d", d3.line()(d));
+
+    d = [];
+    for (var i = 1; i < 9; i += 0.01) {
+        d.push([x2(i), y2(6 / (Math.PI * i) * Math.sin(Math.PI * i / 2))]);
+    }
+    ampline.attr("d", d3.line()(d));
+}
+
+for (var i = 1; i < 10; i++) {
+    d3.select(`#mode${i}`).on("input", update);
+}
+
+update();
