562baf4433946b60c97f0c3e55801572318ca995
Robert Bennett <rltbennett@icloud.com>
2021-01-14 22:53:37 -0500

    Actually got numeric solving of the energy-finding equation more or less working

--- RB/src/demos/Quantum Mechanics/New demos/particle_finite_height_box/particle_finite_height_box.js
+++ RB/src/demos/Quantum Mechanics/New demos/particle_finite_height_box/particle_finite_height_box.js
@@ -13,8 +13,8 @@ const plot = d3.select("#plot").attr("width", WIDTH).attr("height", HEIGHT);
 const xScale = d3.scaleLinear([X_MIN, X_MAX], [0, WIDTH]);
 const yScale = d3.scaleLinear([Y_MIN, Y_MAX], [HEIGHT, 0]);
 
-const m = 1;
-const L = 0.9;
+const m = 1.2;
+const L = 1.2;
 const U = 15;
 const hbar = 0.3;
 
@@ -85,14 +85,73 @@ function energyEquation(E, even) {
 	return E ** 0.5 / trigFunction((L * (2 * m * E) ** 0.5) / (2 * hbar));
 }
 
+function findEnergies2({ even }) {
+	const nPointsSearched = 100000;
+
+	const u0 = ((m * L ** 2 * U) / (2 * hbar ** 2)) ** 0.5;
+	const errorFunc = even
+		? v => Math.abs(v * Math.tan(v) - (u0 ** 2 - v ** 2) ** 0.5)
+		: v => Math.abs(-v / Math.tan(v) - (u0 ** 2 - v ** 2) ** 0.5);
+
+	let energies = [u0 / 2];
+	const N = Math.ceil((2 * u0) / Math.PI);
+	// console.log(even, N);
+
+	for (let n = 0; n < 2; ++n) {
+		const energyCandidates = [...energies];
+		// console.log(energyCandidates);
+		energies = [];
+
+		const searchHalfWidth = (u0 / 2) * nPointsSearched ** -n;
+		for (const ec of energyCandidates) {
+			if (ec > N) {
+				continue;
+			}
+			const EMin = ec - searchHalfWidth;
+			const EMax = ec + searchHalfWidth;
+			const dE = (EMax - EMin) / (nPointsSearched - 1);
+
+			let isDescending = false;
+			let prevEnergy;
+			let prevError = Infinity;
+
+			for (let i = 0; i < nPointsSearched; ++i) {
+				const energy = EMin + (i + 1) * dE;
+				const error = errorFunc(energy);
+
+				if (isDescending && error > prevError) {
+					energies.push(prevEnergy);
+				}
+
+				isDescending = error < prevError;
+				prevEnergy = energy;
+				prevError = error;
+			}
+		}
+	}
+
+	energies.sort((a, b) => a - b);
+	console.log(energies);
+	energies = energies
+		.map(v => (2 * hbar ** 2 * v ** 2) / (m * L ** 2))
+		.filter(energy => energy < U);
+	console.log(energies);
+
+	return energies;
+}
+
 function findEnergies({ even }) {
 	const nPointsSearched = 1000;
 
 	const trigFunction = even ? Math.cos : Math.sin;
-	const energyEquation = E =>
-		E ** 0.5 / trigFunction((L * (2 * m * E) ** 0.5) / (2 * hbar));
-
 	const sqrtU = U ** 0.5;
+	const errorFunc = energy =>
+		Math.abs(
+			sqrtU -
+				energy ** 0.5 /
+					trigFunction((L * (2 * m * energy) ** 0.5) / (2 * hbar)),
+		);
+
 	let energies = [U / 2];
 
 	for (let n = 0; n < 2; ++n) {
@@ -106,46 +165,45 @@ function findEnergies({ even }) {
 			const EMax = ec + precision;
 			const dE = (EMax - EMin) / (nPointsSearched - 1);
 
-			let prevDiffFromU = -Infinity;
-			let currDiffFromU = Infinity;
+			let isDescending = false;
+			let prevEnergy;
+			let prevError = Infinity;
 
-			for (let i = 0; i < nPointsSearched - 1; ++i) {
-				const E = EMin + (i + 1) * dE;
-				const valueThatShouldBeCloseToU = energyEquation(E);
-				const nextDiffFromU = Math.abs(sqrtU - valueThatShouldBeCloseToU);
-				if (currDiffFromU < prevDiffFromU && currDiffFromU < nextDiffFromU) {
-					energies.push(EMin + i * dE);
+			for (let i = 0; i < nPointsSearched; ++i) {
+				const energy = EMin + (i + 1) * dE;
+				const error = errorFunc(energy);
+
+				if (isDescending && error > prevError) {
+					energies.push(prevEnergy);
 				}
 
-				prevDiffFromU = currDiffFromU;
-				currDiffFromU = nextDiffFromU;
+				isDescending = error < prevError;
+				prevEnergy = energy;
+				prevError = error;
 			}
 		}
 	}
 
 	energies.sort((a, b) => a - b);
-	energies = energies.filter(
-		(E, i) =>
-			Math.abs(energyEquation(E) - sqrtU) < 1 &&
-			(i === energies.length - 1 || Math.abs(E - energies[i + 1]) > 1e-4),
-	);
+	energies = energies.filter(energy => errorFunc(energy) < 1e-3);
 
 	return energies;
 }
 
-function update() {
+function update(energy0) {
 	const nPoints = 500;
 	const dx = (X_MAX - X_MIN) / (nPoints - 1);
 	const energyCurves = [];
 
 	for (const even of [true, false]) {
 		const psi = even ? psiEven : psiOdd;
-		const energies = findEnergies({ even });
+		const energies = energy0 !== undefined ? [energy0] : findEnergies2({ even });
 		console.log(even, energies);
 		for (const energy of energies) {
 			const alpha = $alpha(U, energy);
 			const k = $k(energy);
 
+			console.log(energy, alpha, k * Math.tan((k * L) / 2));
 			// console.log(alpha, k, $CEven(alpha, k), $BEven(alpha, k), (k * L) / 2);
 
 			const points = [];
