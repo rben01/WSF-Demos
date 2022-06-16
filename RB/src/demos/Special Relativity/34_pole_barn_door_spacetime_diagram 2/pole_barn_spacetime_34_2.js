c020c116eb2ffcfc04e00bb5a8157014a8403962
Robert Bennett <rltbennett@icloud.com>
2022-06-16 12:35:31 -0400

    Deleted duplicate dirs created by iCloud

--- RB/src/demos/Special Relativity/34_pole_barn_door_spacetime_diagram 2/pole_barn_spacetime_34_2.js
+++
@@ -1,8 +0,0 @@
-/* global update sliders BARN NONE */
-
-update({
-	v: sliders.v.value,
-	t: sliders.t.value,
-	perspective: BARN,
-	slicesToShow: NONE,
-});
--- RB/src/demos/Special Relativity/34_pole_barn_door_spacetime_diagram 2/pole_barn_spacetime_34_3.js
+++
@@ -1,8 +0,0 @@
-/* global update sliders BARN BOTH */
-
-update({
-	v: sliders.v.value,
-	t: sliders.t.value,
-	perspective: BARN,
-	slicesToShow: BOTH,
-});
